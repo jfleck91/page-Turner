39f3ffca2f35d95c50bd3ccb9b84d48ab345770b
Briana Oursler <boursler@blackhole.localdomain>
2018-03-17 13:01:29 -0700

    api key

--- javascript/app.js
+++ javascript/app.js
@@ -1,3 +1,7 @@
+<<<<<<< Updated upstream
 $(document).ready(function(){
     $(".button-collapse").sideNav();
 });
+=======
+var googleBooksApiKey = "AIzaSyA1b6ByAolu8UnLzqSE5KleVdhA572NBRY"
+>>>>>>> Stashed changes

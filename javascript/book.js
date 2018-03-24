fcc047a3ddf383286f33144818267380e038237f
Ricky Hsu <rickyhsu0101@gmail.com>
2018-03-24 11:19:22 -0700

    revert

--- javascript/book.js
+++ javascript/book.js
@@ -1,4 +1,12 @@
+
 $(document).ready(function() {
-var bookString = localStorage.getItem("bookInfo");
-var bookObject = JSON.parse(bookString)
+    var bookString = localStorage.getItem("bookInfo");
+    var bookObject = JSON.parse(bookString);
+    $("#bookImage").attr("src", bookObject.imageLinks.thumbnail);
+    $("#title").text(bookObject.title);
+    authors = "";
+    bookObject.authors.forEach(function (authorName) {
+        authors += authorName + ', ';
+    });
+    $("#authors").text(authors);
 });

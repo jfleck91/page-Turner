03cb75b51d61903e1e156c28a81c24965c03f808
Ricky Hsu <rickyhsu0101@gmail.com>
2018-03-24 12:36:35 -0700

    fixed search fields

--- javascript/home.js
+++ javascript/home.js
@@ -2,6 +2,7 @@
 $('.carousel.carousel-slider').carousel({ fullWidth: true });
 
 $(document).ready(function () {
+    $(".button-collapse").sideNav();
     $('.carousel').carousel({ dist: 0 });
     window.setInterval(function () { $('.carousel').carousel('next') }, 5000)
 });
@@ -11,7 +12,12 @@ $(".btn-floating2").click(function () {
         scrollTop: $("#bottom").offset().top
     }, 1500);
 });
-
+function addSessionStorage(query){
+    sessionStorage.setItem("query", query);
+    open("./html/search.html", "_self");
+}
 $(document).on("keyup", function(e){
-    console.log(e.key);
+    if((e.key === "Enter")&& $("#querySearch").val().length > 0){
+        addSessionStorage($("#querySearch").val());
+    }
 });
